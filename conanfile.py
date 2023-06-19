581cf3f73e8bc6e795bab677143e5d1e87ceeca3
Nino Maruszewski <nino.maruszewski@gmail.com>
2023-06-18 20:10:19 -0500

    add deps

--- conanfile.py
+++ conanfile.py
@@ -12,5 +12,15 @@ class Recipe(ConanFile):
         self.requires("fmt/10.0.0")
         self.requires("quill/2.9.2")
 
+        self.requires("libwebsockets/4.3.2")  # Brings in libuv too
+        self.requires("glaze/1.2.6")
+
+        self.requires("hiredis/1.1.0")
+
     def build_requirements(self):
         self.test_requires("catch2/3.3.1")
+
+    def configure(self):
+        # This is for libwebsockets but for some reason doesn't work with
+        # self.options['libwebsockets']
+        self.options["*"].with_libuv = True
