49a27a485bde370b68be2efc4449d6ad8831c82d
stevenewald <stevenewald6@gmail.com>
2023-07-16 22:42:57 -0400

    Formatting

--- src/storage/processing.hpp
+++ src/storage/processing.hpp
@@ -36,7 +36,9 @@ private:
     void process_incoming_snapshot(const ObSnapshot& newOb);
     void process_incoming_update(const Update& newUpdate);
     void ob_to_redis(std::string product_id);
-    void map_to_redis(const std::unordered_map<double, double>& table, const std::string& map_id);
+    void map_to_redis(
+        const std::unordered_map<double, double>& table, const std::string& map_id
+    );
 };
 
 } // namespace storage
