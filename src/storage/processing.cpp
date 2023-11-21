9f052009fd54c8ed5d8990ad7790a55b88186281
Steve <stevenewald6@gmail.com>
2023-11-21 16:57:03 +0000

    Improve data processing by using decay_t and constexpr/is_same_v (#29)
    Co-authored-by: Nikola Maruszewski <me@nikola.cx>

--- src/storage/processing.cpp
+++ src/storage/processing.cpp
@@ -17,31 +17,32 @@ DataProcessor::process_incoming_data(const std::string& json_data)
         return;
     }
 
-    // Check what kind of data we got
-    if (std::holds_alternative<OrderbookUpdate>(data)) {
-        // We got an update
-        const auto& update = std::get<OrderbookUpdate>(data);
+    auto processMessage = [&](const auto& message) {
+        using T = std::decay_t<decltype(data)>;
+        // Check what kind of data we got
+        if constexpr (std::is_same_v<T, OrderbookUpdate>) {
+            // We got an update
+            orderbook_prox_.process_incoming_update(message);
+            orderbook_prox_.ob_to_redis(redis_, message.product_id);
+        }
+        else if constexpr (std::is_same_v<T, OrderbookSnapshot>) {
+            // We got a snapshot
+            orderbook_prox_.process_incoming_snapshot(message);
+            orderbook_prox_.ob_to_redis(redis_, message.product_id);
+        }
+        else if constexpr (std::is_same_v<T, Match>) {
+            // We got a match
+            const auto& match = std::get<Match>(data);
 
-        orderbook_prox_.process_incoming_update(update);
-        orderbook_prox_.ob_to_redis(redis_, update.product_id);
-    }
-    else if (std::holds_alternative<OrderbookSnapshot>(data)) [[unlikely]] {
-        // We got a snapshot
-        const auto& snapshot = std::get<OrderbookSnapshot>(data);
+            trade_prox_.process_incoming_match(match);
+            trade_prox_.matches_to_redis(redis_);
+        }
+        else {
+            log_e(main, "Unknown data type");
+        }
+    };
 
-        orderbook_prox_.process_incoming_snapshot(snapshot);
-        orderbook_prox_.ob_to_redis(redis_, snapshot.product_id);
-    }
-    else if (std::holds_alternative<Match>(data)) {
-        // We got a match
-        const auto& match = std::get<Match>(data);
-
-        trade_prox_.process_incoming_match(match);
-        trade_prox_.matches_to_redis(redis_);
-    }
-    else [[unlikely]] {
-        log_e(main, "Unknown data type");
-    }
+    std::visit(processMessage, data);
 }
 
 } // namespace storage
